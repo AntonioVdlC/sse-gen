602dc8d89ac785be704ef154cfe6386458c4a1a5
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2024-04-23 22:27:55 +0800

    Add docs

--- README.md
+++ README.md
@@ -1,2 +1,147 @@
 # sse-gen
 
+[![version](https://img.shields.io/npm/v/sse-gen.svg)](http://npm.im/sse-gen)
+[![issues](https://img.shields.io/github/issues-raw/antoniovdlc/sse-gen.svg)](https://github.com/sse-gen/issues)
+[![downloads](https://img.shields.io/npm/dt/sse-gen.svg)](http://npm.im/sse-gen)
+[![license](https://img.shields.io/npm/l/sse-gen.svg)](http://opensource.org/licenses/MIT)
+
+A Server-Sent Event (SSE) client using Generators.
+
+## Installation
+
+This package is distributed via npm:
+
+```
+npm install sse-gen
+```
+
+## Methods
+
+### Instantiating a client
+
+To instantiate a client, call `new` on the class, and pass it a `url` value.
+
+```ts
+const client = new SSEClient<T>("/events");
+```
+
+#### options
+
+An optional `options` object can be passed as a second argument.
+
+```ts
+type SSEClientOptions = {
+  onStatusUpdate?: StatusUpdateHandler;
+  reconnect?: { maxAttempts: number; delay: number };
+};
+```
+
+The use of those options is explained further down this document.
+
+### Connecting to the server
+
+To connect to the server (based on the URL passed to the constructor), call the `connect` method on the instantiated client.
+
+```ts
+client.connect();
+```
+
+### Closing the connectino to the server
+
+To close the connect to the server, call the `close` method on the instantiated client.
+
+```ts
+client.close();
+```
+
+### Handle SSE messages
+
+To add an handled for messages sent by the server, pass an `EventHandlerMessage` function to the `on` method.
+
+```ts
+client.on((event) => {
+  console.log(event.data);
+});
+```
+
+Note that if you are receiving a JSON string, you might need to parse it using `JSON.parse()`.
+
+#### Asynchronous handler
+
+The main motivation of using a Generator to handle events in this SSE client is the possibility to have asynchronous message handlers. Using a Generator ensures that messages will be processed one by one, and that new messages will wait for the previous messages to be processed.
+
+### Error handling
+
+#### Automatic reconnection
+
+If passed a `reconnect` option to the constructor, the client will try to reconnect automatically.
+
+The number of times the client will try to reconnect is defined by the `maxAttempts` property of the `reconnect` option. The delay between each reconnection attempt is defined by the `delay` property of the `reconnect` option.
+
+```ts
+const client = new SSEClient("/events", {
+  reconnect: {
+    maxAttempts: 3,
+    delay: 1000,
+  },
+});
+```
+
+#### Custom error handling
+
+To add further custom error handling, pass an `EventHandlerError` function to the `catch` method.
+
+```ts
+client.catch((event) => {
+  console.error(event);
+});
+```
+
+Note that the client calls `close` when receiving an error.
+
+## Getters
+
+### url
+
+To retrieve the URL the client is connected to, use the `url` getter on the instantiated client.
+
+```ts
+const client = new SSEClient<T>("/events");
+client.url; // "/events"
+```
+
+### status
+
+The client can be in different statuses:
+
+```ts
+enum SSEClientStatus {
+  CONNECTING = "CONNECTING",
+  OPEN = "OPEN",
+  CLOSED = "CLOSED",
+}
+```
+
+To retrieve the status of the client, use the `status` getter on the instantiated client.
+
+```ts
+const client = new SSEClient<T>("/events");
+client.connect();
+client.status; // "CONNECTING"
+// ...
+client.status; // "OPEN"
+// ...
+client.close();
+client.status; // "CLOSED"
+```
+
+#### Custom status update handler
+
+You can also pass a custom handler for status updates in the constructor.
+```ts
+const client = new SSEClient("/events", {
+  onStatusUpdate: (status) => {
+    console.log(status);
+  },
+});
+```
--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,195 @@
+/**
+ * Server-Sent Event message handler.
+ *
+ * @template T - The type of the data sent by the server.
+ *
+ * @param {MessageEvent<T>} event - The message event.
+ * @returns {void | Promise<void>}
+ */
 type EventHandlerMessage<T> = (event: MessageEvent<T>) => void | Promise<void>;
+/**
+ * Server-Sent Event error handler.
+ *
+ * @param {Event} event - The error event.
+ * @returns {void}
+ */
 type EventHandlerError = (event: Event) => void;
 
+/**
+ * Server-Sent Event message event generator.
+ *
+ * @template T - The type of the data sent by the server.
+ *
+ * @type {AsyncGenerator<MessageEvent<T> | undefined, void, MessageEvent<T>>}
+ * @yields {MessageEvent<T> | undefined} - The message event.
+ * @returns {void}
+ */
 type EventHandlerGenerator<T> = AsyncGenerator<
   MessageEvent<T> | undefined,
   void,
   MessageEvent<T>
 >;
 
+/**
+ * SSE client status
+ *
+ * @enum {string}
+ * @property {string} CONNECTING - The client is connecting to the server.
+ * @property {string} OPEN - The client is connected to the server.
+ * @property {string} CLOSED - The client is disconnected from the server.
+ */
 enum SSEClientStatus {
   CONNECTING = "CONNECTING",
   OPEN = "OPEN",
   CLOSED = "CLOSED",
 }
+/**
+ * Server-Sent Event status update handler.
+ *
+ * @param {SSEClientStatus} status - The status of the client.
+ * @returns {void}
+ */
+type StatusUpdateHandler = (status: SSEClientStatus) => void;
 
+/**
+ * Server-Sent Event client options.
+ *
+ * @property {StatusUpdateHandler} [onStatusUpdate] - The status update event handler.
+ * @property {{ maxAttempts: number; delay: number }} [reconnect] - The reconnect options.
+ * @property {number} reconnect.maxAttempts - The maximum number of reconnect attempts.
+ * @property {number} reconnect.delay - The delay between reconnect attempts.
+ */
+type SSEClientOptions = {
+  onStatusUpdate?: StatusUpdateHandler;
+  reconnect?: { maxAttempts: number; delay: number };
+};
+
+/**
+ * Server-Sent Events (SSE) client.
+ *
+ * @template T - The type of the data sent by the server.
+ *
+ * @constructor
+ * @param {string} url - The URL of the server.
+ * @param {{ onStatusUpdate?: StatusUpdateHandler; reconnect?: { maxAttempts: number; delay: number } }} [options] - The options.
+ * @param {onStatusUpdate} [options.onStatusUpdate] - The status update event handler.
+ * @param {reconnect} [options.reconnect] - The reconnect options.
+ * @param {number} options.reconnect.maxAttempts - The maximum number of reconnect attempts.
+ * @param {number} options.reconnect.delay - The delay between reconnect attempts.
+ *
+ * @property {string} url - The URL of the server.
+ * @property {SSEClientStatus} status - The status of the client.
+ *
+ * @method connect - Connect to the server.
+ * @method close - Close the connection.
+ * @method catch - Set the error event handler.
+ * @method on - Set the message event handler.
+ *
+ * @example
+ * const client = new SSEClient("/events", {
+ *  onStatusUpdate: (status) => {
+ *    console.log(status);
+ *  },
+ *  reconnect: {
+ *    maxAttempts: 3,
+ *    delay: 1000,
+ *  },
+ * });
+ *
+ * client.connect();
+ *
+ * client.on((event) => {
+ *  console.log(event.data);
+ * });
+ *
+ * client.catch((event) => {
+ *  console.error(event);
+ * });
+ *
+ * client.close();
+ *
+ */
 class SSEClient<T> {
+  /**
+   * @protected
+   * @type {string} _url - The URL of the server.
+   */
   protected _url: string;
+  /**
+   * @protected
+   * @type {EventSource | null} _eventSource - The event source.
+   */
   protected _eventSource: EventSource | null;
 
+  /**
+   * @private
+   * @type {EventHandlerMessage<T> | undefined} _handleMessage - The message event handler.
+   */
   private _handleMessage?: EventHandlerMessage<T>;
+  /**
+   * @private
+   * @type {EventHandlerError | undefined} _handleError - The error event handler.
+   */
   private _handleError?: EventHandlerError;
 
+  /**
+   * @private
+   * @type {EventHandlerGenerator<T>} _eventHandler - The event handler generator.
+   */
   private _eventHandler: EventHandlerGenerator<T>;
 
+  /**
+   * @protected
+   * @type {SSEClientStatus} _status - The status of the client.
+   */
   protected _status: SSEClientStatus = SSEClientStatus.CLOSED;
-  private _onStatusUpdate?: (status: SSEClientStatus) => void;
+  /**
+   * @private
+   * @type {StatusUpdateHandler} _onStatusUpdate - The status update event handler.
+   */
+  private _onStatusUpdate?: StatusUpdateHandler;
 
+  /**
+   * @protected
+   * @type {{ attempts: number; maxAttempts: number; delay: number } | undefined} _reconnect - The reconnect options.
+   * @property {number} _reconnect.attempts - The number of reconnect attempts.
+   * @property {number} _reconnect.maxAttempts - The maximum number of reconnect attempts.
+   * @property {number} _reconnect.delay - The delay between reconnect attempts.
+   */
   protected _reconnect?: {
     attempts: number;
     maxAttempts: number;
     delay: number;
   };
 
+  /**
+   * @getter
+   * @type {string} url - The URL of the server.
+   */
   get url(): string {
     return this._url;
   }
 
+  /**
+   * @getter
+   * @type {SSEClientStatus} status - The status of the client.
+   */
   get status(): SSEClientStatus {
     return this._status;
   }
 
-  constructor(
-    url: string,
-    options?: {
-      onStatusUpdate?: (status: SSEClientStatus) => void;
-      reconnect?: { maxAttempts: number; delay: number };
-    },
-  ) {
+  /**
+   * @constructor
+   * @param {string} url - The URL of the server.
+   * @param {SSEClientOptions} [options] - The options.
+   * @returns {SSEClient<T>} - The SSE client.
+   */
+  constructor(url: string, options?: SSEClientOptions) {
     this._url = url;
     this._eventSource = null;
 
     this._eventHandler = this._createEventHandler();
-    this._eventHandler.next();
+    this._eventHandler.next(); // Initialise the generator.
 
     this._onStatusUpdate = options?.onStatusUpdate;
 
@@ -62,6 +201,11 @@ class SSEClient<T> {
     }
   }
 
+  /**
+   * Connect to the server.
+   * Add an event listener for the open, message, and error events.
+   * @returns {void}
+   */
   connect(): void {
     this._eventSource = new EventSource(this._url);
     this._updateStatus(SSEClientStatus.CONNECTING);
@@ -84,6 +228,10 @@ class SSEClient<T> {
     };
   }
 
+  /**
+   * Close the connection if it is open.
+   * @returns {void}
+   */
   close(): void {
     if (this._eventSource) {
       this._eventSource.close();
@@ -92,14 +240,40 @@ class SSEClient<T> {
     }
   }
 
+  /**
+   * Set the error event handler.
+   * @param {EventHandlerError} handleError - The error event handler.
+   * @returns {void}
+   *
+   * @example
+   * client.catch((event) => {
+   *  console.error(event);
+   * });
+   */
   catch(handleError: EventHandlerError): void {
     this._handleError = handleError;
   }
 
+  /**
+   * Set the message event handler.
+   * @param {EventHandlerMessage<T>} handleMessage - The message event handler.
+   * @returns {void}
+   *
+   * @example
+   * client.on((event) => {
+   *  console.log(event.data);
+   * });
+   */
   on(handleMessage: EventHandlerMessage<T>): void {
     this._handleMessage = handleMessage;
   }
 
+  /**
+   * Create the event handler generator.
+   * @returns {EventHandlerGenerator<T>} - The event handler generator.
+   * @yields {MessageEvent<T> | undefined} - The message event.
+   * @returns {void}
+   */
   private async *_createEventHandler(): EventHandlerGenerator<T> {
     while (true) {
       const event = yield;
@@ -109,11 +283,21 @@ class SSEClient<T> {
     }
   }
 
+  /**
+   * Update the status of the client.
+   * Call the status update event handler.
+   * @param {SSEClientStatus} status - The status of the client.
+   * @returns {void}
+   */
   protected _updateStatus(status: SSEClientStatus): void {
     this._status = status;
     this._onStatusUpdate?.(status);
   }
 
+  /**
+   * Attempt to reconnect to the server.
+   * @returns {void}
+   */
   protected _handleReconnect(): void {
     if (this._reconnect) {
       if (this._reconnect.attempts < this._reconnect.maxAttempts) {
@@ -127,4 +311,4 @@ class SSEClient<T> {
 }
 
 export { SSEClient, SSEClientStatus };
-export type { EventHandlerMessage, EventHandlerError };
+export type { EventHandlerMessage, EventHandlerError, StatusUpdateHandler };
